10abdff11812a96a9437ac9e2451eddf3c96f2fc
fuwa <fuwafuwa41.glr@gmail.com>
2021-12-08 09:16:33 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,9 @@
 class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page])
-    render :index
   end
 
   def show
     @user = User.find(params[:id])
-    render :show
   end
 end

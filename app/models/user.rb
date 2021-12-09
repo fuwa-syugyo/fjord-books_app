f7eba47903f03594260b2066eb6bd1165e0f2ced
fuwa <fuwafuwa41.glr@gmail.com>
2021-12-09 09:47:01 +0900

    image?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def validate_icon
-    return unless avatar.attached?
+    return unless avatar.image?
 
     unless validate_extension
       avatar = nil

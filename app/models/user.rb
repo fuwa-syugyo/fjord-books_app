d835c9ea46242e73e44873779e48aff3c8f4cbe5
fuwa <fuwafuwa41.glr@gmail.com>
2021-12-13 09:26:26 +0900

    make user_follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_user_follows, class_name: 'UserFollow',
+                                 foreign_key: 'follower_id',
+                                 dependent: :destroy,
+                                 inverse_of: :follower
+  has_many :passive_user_follows, class_name: 'UserFollow',
+                                  foreign_key: 'followed_id',
+                                  dependent: :destroy,
+                                  inverse_of: :followed
+  has_many :following, through: :active_user_follows, source: :followed
+  has_many :followers, through: :passive_user_follows, source: :follower
+
+  paginates_per 10
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_user_follows.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end

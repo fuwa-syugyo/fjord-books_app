71dda33b5dee57c6938b45f4be1c3b06a0a5ee41
fuwa <fuwafuwa41.glr@gmail.com>
2021-12-31 13:46:03 +0900

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,7 @@ class CommentsController < ApplicationController
   # GET /comments or /comments.json
   def index
     @comments = @commentable.comments
-    # @author = User.find(@comment.user_id)
   end
-
-  # GET /comments/1 or /comments/1.json
-  def show; end
 
   # GET /comments/new
   def new
@@ -20,34 +16,12 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
-  def edit
-    @report = Report.find(params[:report_id])
-    @book = Book.find(params[:book_id])
-    @comment = Comment.find(params[:id])
-  end
-
   # POST /comments or /comments.json
   def create
     @comment = @commentable.comments.new comment_params
     @comment.user = current_user
     @comment.save
     redirect_to @commentable, notice: t('views.common.your_comment_was_successfully_posted')
-  end
-
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    if @comment.update(comment_params)
-      redirect_to report_comments_path, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private

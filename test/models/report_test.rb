8aba6229476c2bac96df992c63ec430f1aa24d20
fuwa <fuwafuwa41.glr@gmail.com>
2022-01-15 16:59:43 +0900

    add test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    report = reports(:test_report)
+
+    user = users(:alice)
+    assert_equal true, report.editable?(user)
+
+    user = users(:bob)
+    assert_equal false, report.editable?(user)
+  end
 end
--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,55 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:cherry_book)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+    visit root_url
+    assert_selector 'h1', text: '本'
   end
 
   test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
+    visit root_url
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: 'Ruby超入門'
+    fill_in 'メモ', with: 'Rubyの文法の基本をやさしくていねいに解説しています。'
+    fill_in '著者', with: '五十嵐 邦明'
+    click_button '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    assert_text 'Ruby超入門'
+    assert_text 'Rubyの文法の基本をやさしくていねいに解説しています。'
+    assert_text '五十嵐 邦明'
   end
 
   test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
+    visit root_url
+    click_on '編集', match: :prefer_exact
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_button '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text 'チェリー本'
+    assert_text 'プログラミング経験者のためのRuby入門書です。'
+    assert_text '伊藤 淳一'
+    click_on '戻る'
   end
 
   test 'destroying a Book' do
-    visit books_url
+    visit root_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
